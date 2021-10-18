d60a325400a92c4045d62c2539497645329dba11
PauloVLB <vitorlimaborges@gmail.com>
2021-10-18 14:50:14 -0300

    aceita url global

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, 
     {
         cors: {
-            origin: process.env.FRONT_URL,
+            origin: '*',
             methods: ["GET", "POST"]
         }
     }
